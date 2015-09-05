8318a269fae4d6986d4d273dfb8d5071728c72e8
anhowe <ahowe_ca@yahoo.ca>
2015-09-05 15:28:54 -0700

    fix script

--- configure-ubuntu.sh
+++ configure-ubuntu.sh
@@ -8,6 +8,9 @@ echo "starting ubuntu devbox install on pid $$"
 ps axjf
 
 sudo apt-get -y update
+# kill the waagent and uninstall, otherwise, adding the desktop will do this and kill this script
+sudo pkill waagent
+sudo apt-get -y remove walinuxagent
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install ubuntu-desktop firefox vnc4server ntp nodejs npm expect gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal gnome-core
 
 #########################################
