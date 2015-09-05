572074b1a15f4f0850d1a70f8a372bc5cc6e6ff3
anhowe <ahowe_ca@yahoo.ca>
2015-09-05 14:12:51 -0700

    script fixes

--- configure-ubuntu-bootstrap.sh
+++ configure-ubuntu-bootstrap.sh
@@ -3,5 +3,4 @@
 #########################################################
 # Start the Ubuntu configuration and finish immediately
 #########################################################
-wget -qO- https://raw.githubusercontent.com/anhowe/ubuntu-devbox/master/configure-ubuntu.sh | nohup /bin/bash &
-return 0
+wget -qO- https://raw.githubusercontent.com/anhowe/ubuntu-devbox/master/configure-ubuntu.sh | nohup /bin/bash > /var/log/azure/firstinstall.log 2>&1 &
