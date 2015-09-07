9a59a89dd538992b687dc9b567af2f9f95fffc03
anhowe <ahowe_ca@yahoo.ca>
2015-09-07 06:16:31 -0700

    add retries to wget command

--- configure-ubuntu-bootstrap.sh
+++ configure-ubuntu-bootstrap.sh
@@ -3,4 +3,4 @@
 #########################################################
 # Start the Ubuntu configuration and finish immediately
 #########################################################
-wget -qO- https://raw.githubusercontent.com/anhowe/ubuntu-devbox/master/configure-ubuntu.sh | nohup /bin/bash > /var/log/azure/firstinstall.log 2>&1 &
+wget --tries 20 --retry-connrefused --waitretry=15 -qO- https://raw.githubusercontent.com/anhowe/ubuntu-devbox/master/configure-ubuntu.sh | nohup /bin/bash > /var/log/azure/firstinstall.log 2>&1 &
