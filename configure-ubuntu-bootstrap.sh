45eb90b705250bc6ed8d0637cc372bfeb61486ef
anhowe <ahowe_ca@yahoo.ca>
2015-09-07 07:44:46 -0700

    Remove network dependency from script extension
    The network is flakey causing script extension to fail unpredictably.
    This adjustment removes the network dependency from the script
    extension, and then the script takes care of the retries outside of the
    guest agent.
    
    This approach also allows removal of the bootstrap script.
    
    Additionally "devbox" was renamed to "jumpbox".

--- configure-ubuntu-bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#########################################################
-# Start the Ubuntu configuration and finish immediately
-#########################################################
-wget --tries 20 --retry-connrefused --waitretry=15 -qO- https://raw.githubusercontent.com/anhowe/ubuntu-devbox/master/configure-ubuntu.sh | nohup /bin/bash > /var/log/azure/firstinstall.log 2>&1 &
